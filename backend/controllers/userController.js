0f114b268b45ea93fb35f967bd2d3c3ee6b11039
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-11 19:47:54 +0800

    @create registerUser function to create a user  into the db.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,40 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+/**
+ * @desc Register a user.
+ * @route POST /api/users
+ */
+
+export const registerUser = async (request, response) => {
+  console.log("request: ", request.body);
+
+  // Destructure the data
+  const { name, email, password } = request.body;
+  // Validate the request body
+
+  // Await the finding of the user before proceeding.
+  const userExist = await User.findOne({ email });
+  console.log("userExist: ", userExist);
+
+  // If user exists, do not create and return nothing to cancel the creation.
+  if (userExist) {
+    response.status(400).json({ message: "User already exists." });
+    return false;
+  }
+
+  // If user doesn't exists, create one.
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // If successfully created the user, respond something. Else, status to 400.
+  if (user) {
+    generateToken(response, user._id);
+    response.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    response.status(400);
+  }
+};
