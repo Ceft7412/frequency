87960ad1413d38ba01a6884216753d710b0c89b0
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-11 20:31:57 +0800

    @update logoutUser and loginUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,41 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
+
+export const logoutUser = async (request, response) => {
+  response.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  response.status(200).json({ message: "User logged out" });
+};
+
+/**
+ * @desc Login a user.
+ * @route POST /api/users/signin
+ */
+export const authUser = async (request, response) => {
+  // Destructure the data and get only the email and password.
+  const { email, password } = request.body;
+
+  // Find a user the has the similar entered email in the database.
+  const user = await User.findOne({ email });
+
+  // If no user is found, or the password doesn't match, send a 401 response.
+  if (!user || !(await user.matchPassword(password))) {
+    response.status(401).json({ message: "Invalid email or password" });
+    return;
+  }
+  // If the user is found and the password matches, generate a token and send a 201 response.
+  generateToken(response, user._id);
+  response.status(201).json({ _id: user._id, name: user.name, email: user.email });
+};
+
 /**
  * @desc Register a user.
- * @route POST /api/users
+ * @route POST /api/users/register
  */
-
 export const registerUser = async (request, response) => {
   console.log("request: ", request.body);
 
