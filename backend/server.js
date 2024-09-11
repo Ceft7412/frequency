6670ce2883425afe9b213def3b6f2b18875a161e
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-11 21:13:43 +0800

    @update to use the cookie parser in the server.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import router from "./routes/routeBlogs.js";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
+import blogsRouter from "./routes/blogRoute.js";
 
 dotenv.config();
 
@@ -11,7 +13,18 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// User router
+app.use("/api/users/", userRoute);
+
+// Blog router
+// app.use("/api/", router);
+// app.use("/api/blogs", router);
+// app.use("/api/blogs/35/blog", router);
+
+// Blog router
+app.use("/api/blogs/", blogsRouter);
 const PORT = process.env.PORT;
 const ENVIRONMENT = process.env.NODE_ENVIRONMENT;
 console.log("ENVIRONMENT: ", ENVIRONMENT);
@@ -20,12 +33,3 @@ console.log("PORT: ", PORT);
 app.listen(PORT, () => {
   console.log("Frequency App");
 });
-
-// User router
-app.use("/api/users/", userRoute);
-
-// Blog router
-// app.use("/api/", router);
-
-// app.use("/api/blogs", router);
-// app.use("/api/blogs/35/blog", router);
