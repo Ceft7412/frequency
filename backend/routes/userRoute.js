9222d90fcafa829847a3400543a42698e27f1e48
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-11 21:13:06 +0800

    @import protect route.

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,9 +2,10 @@ import express from "express";
 import { authUser, logoutUser, registerUser } from "../controllers/userController.js";
 const userRoute = express.Router();
 
-
+import { protect } from "../middleware/authMiddleware.js";
 
 userRoute.post("/register", registerUser);
 userRoute.post("/signin", authUser);
 userRoute.post("/logout", logoutUser);
+
 export default userRoute;
