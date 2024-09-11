2d6189d5acaba9ed15d8e1e067927f9ffc37f38d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-11 19:48:38 +0800

    @create register endpoint where the user can request to create a user

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { registerUser } from "../controllers/userController.js";
+const userRoute = express.Router();
+
+userRoute.post("/register", registerUser);
+export default userRoute;
