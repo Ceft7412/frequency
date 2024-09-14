3f44c6b32b0df96b06c2cf56b487330f70847f0a
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-14 18:34:32 +0800

    @create Create component

---
+++ frontend/src/screens/auth/Create.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Create() {
+  const { setRouteText } = React.useContext(AuthContext);
+  React.useEffect(() => {
+    setRouteText("Create");
+  });
+  return (
+    <>
+      <AuthLayout>
+        <main className="pl-64">create</main>
+      </AuthLayout>
+    </>
+  );
+}
