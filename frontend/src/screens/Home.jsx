fa89e1699afd6f0d40feea74fb805eed2d37fe88
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-09-17 20:17:33 +0800

    @create home screen

---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GuestHeader from "../components/headers/GuestHeader";
+export default function Home() {
+  return (
+    <>
+      <GuestHeader />
+      <main className="max-w-screen-xl mx-auto flex items-center justify-center py-4 px-2">
+        
+      </main>
+    </>
+  );
+}
